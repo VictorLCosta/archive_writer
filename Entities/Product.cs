29b7c8aeb65ae18d44b0c873212f47a43523ba23
VictorLCosta <victorlc2019@outlook.com>
2020-03-31 23:11:30 -0300

    Tentando criar arquivos com StreamWriter, ps.: É difícil

---
+++ Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ArchiveWriter.Entities
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total() 
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Name}, {Total().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Globalization;
+using ArchiveWriter.Entities;
 
 namespace ArchiveWriter
 {
@@ -7,17 +9,39 @@ namespace ArchiveWriter
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Victor\Desktop\Projetos C#\summary.csv";
+            Console.Write("Enter full file path: ");
+            string source_path = Console.ReadLine();
+            string[] lines = File.ReadAllLines(source_path);
 
-            Console.WriteLine("Get directory separator char: " + Path.DirectorySeparatorChar);
-            Console.WriteLine("Get path separator: " + Path.PathSeparator);
-            Console.WriteLine("Get directory name: " + Path.GetDirectoryName(path));
-            Console.WriteLine("Get file name: " + Path.GetFileName(path));
-            Console.WriteLine("Get file extension: " + Path.GetExtension(path));
-            Console.WriteLine("Get file name without extension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("Get full path: " + Path.GetFullPath(path));
-            Console.WriteLine("Get temp path: " + Path.GetTempPath()); 
-            
+            try 
+            {
+                
+                string source_folder = Path.GetDirectoryName(source_path);
+                string target_folder = source_folder + @"\out";
+                string target_file = target_folder + @"\summary.csv";
+
+                Directory.CreateDirectory(target_folder);
+
+                using (StreamWriter sw = File.AppendText(target_file)) 
+                {
+                    foreach (string line in lines) 
+                    {
+                        string[] fields = line.Split(",");
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        Console.WriteLine(prod);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
